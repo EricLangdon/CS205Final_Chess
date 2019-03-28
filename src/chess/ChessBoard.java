94f407bbc593eb23d87b34c40bc336ee0644dbcf
luke_faulkner <lhfaulkn@uvm.edu>
2019-03-28 17:01:16 -0400

    Readding classes

---
+++ src/chess/ChessBoard.java
@@ -0,0 +1,66 @@
+package chess;
+import java.util.ArrayList;
+
+public class ChessBoard {
+    public static final int NUM_ROWS = 8;
+    public static final int NUM_COLS = 8;
+
+    ArrayList<ArrayList<BoardSquare>> board = new ArrayList<>(NUM_ROWS);
+    ArrayList<Piece> captured = new ArrayList<>();
+    ArrayList<Piece> pieces = new ArrayList<>();
+
+
+    /**
+     * Default Constructor
+     */
+    ChessBoard () {
+        createBoard(board);
+    }
+
+    /**
+     * createBoard
+     */
+    private void createBoard(ArrayList<ArrayList<BoardSquare>> board) {
+        //TODO
+    }
+
+    /**
+     * getCaptured
+     * @return an arraylist of the pieces that have been taken out of play
+     */
+    public ArrayList<Piece> getCaptured() {
+        return captured;
+    }
+
+    /**
+     * getBoardSquareAt
+     * @param x the x-coor of the BoardSquare
+     * @param y the y-coor of the BoardSquare
+     * @return the BoardSquare at (x,y)
+     */
+    public BoardSquare getBoardSquareAt(int x, int y) {
+        //TODO: verify correctness?
+        return board.get(x).get(y);
+    }
+
+    /**
+     * movePiece
+     * @param source where the piece is coming from
+     * @param target where the piece is trying to move
+     * @return true if the piece is successfully moved, false if the move failed
+     */
+    public boolean movePiece(BoardSquare source, BoardSquare target) {
+        // TODO: implement
+        return false;
+    }
+
+    /**
+     * colorInCheck
+     * @param color
+     * @return true if the player of 'color' is in check
+     */
+    public boolean colorInCheck(Color color) {
+        //TODO: implement
+        return false;
+    }
+}
---
+++ src/chess/Queen.java
@@ -0,0 +1,30 @@
+package chess;
+
+import java.util.ArrayList;
+
+public class Queen extends Piece {
+    public static int SCORE = 9;
+
+    /**
+     * legalMove
+     * Check if the given move is legal for the Bishop given the board source and target squares.
+     * @param board the board
+     * @param source the boardsquare containing the piece checking for move
+     * @param target the boardsquare that the piece wants to be moved to
+     * @return boolean true if move is legal
+     */
+    public boolean legalMove (ChessBoard board, BoardSquare source, BoardSquare target){
+        // TODO: implement
+    }
+
+    /**
+     * getAvailableMoves
+     * Returns list of all legal moves available to the Bishop
+     * @param board the board
+     * @param source boardsquare where the piece currently is located
+     * @return an arraylist of all boardsquares that the Bishop can legally move to
+     */
+    public ArrayList<BoardSquare> getAvailableMoves(ChessBoard board, BoardSquare source){
+        // TODO: implement
+    }
+}
