b722f1e2f38e095d18fc56f3de0c7036a0492de3
Eben Collins <collinseben@gmail.com>
2019-04-04 18:35:26 -0400

    remove addPiece and removePiece methods because duplicate functionality of setPiece

--- src/chess/BoardSquare.java
+++ src/chess/BoardSquare.java
@@ -110,21 +110,6 @@ public class BoardSquare {
     public void setHighlighted(boolean highlighted) {
         this.highlighted = highlighted;
     }
-
-
-    /**
-     * addPiece
-     * @param piece the piece occupying the boardSquare
-     */
-    public void addPiece(Piece piece) {
-        this.piece = piece;
-    }
-
-    /**
-     * removePiece
-     */
-    public void removePiece() {
-        this.piece = null;
-    }
+    
 
 }
