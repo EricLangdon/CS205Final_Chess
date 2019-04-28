fb501f7e60270340bac44c4b25d654376f41069b
luke_faulkner <lhfaulkn@uvm.edu>
2019-04-28 19:55:02 -0400

    Added comments

--- src/chess/core/game/cpu/CPU.java
+++ src/chess/core/game/cpu/CPU.java
@@ -9,29 +9,51 @@ public abstract class CPU {
     public void choiceMove(Board board) {
 
     }
-
     class MoveScore {
         BoardSquare source;
         BoardSquare target;
         int score;
 
+
         MoveScore() {
         }
 
+        /**
+         * Non-Defauot constructor
+         *
+         * @param source where the piece was
+         * @param target where the piece is placed
+         * @param score the score of the move
+         */
         MoveScore(BoardSquare source, BoardSquare target, int score) {
             this.source = source;
             this.target = target;
             this.score = score;
         }
 
+        /**
+         * getScore
+         *
+         * @return the score of the move
+         */
         public int getScore() {
             return score;
         }
 
+        /**
+         * getTarget
+         *
+         * @return the target boardsquare
+         */
         public BoardSquare getTarget() {
             return target;
         }
 
+        /**
+         * getSource
+         *
+         * @return the source boardsquare
+         */
         public BoardSquare getSource() {
             return source;
         }
@@ -44,25 +66,51 @@ public abstract class CPU {
         TargetScore() {
         }
 
+        /**
+         * Non-default constructor
+         *
+         * @param target boardsquare
+         * @param score of the target
+         */
         TargetScore(BoardSquare target, int score) {
             this.target = target;
             this.score = score;
         }
 
+        /**
+         * getScore
+         *
+         * @return the score of the target
+         */
         public int getScore() {
             return score;
         }
 
+        /**
+         * getTarget
+         *
+         * @return boardsquare location of the target
+         */
         public BoardSquare getTarget() {
             return target;
         }
 
+        /**
+         * setScore
+         *
+         * @param score of the target
+         */
         public void setScore(int score) {
             this.score = score;
         }
+
+        /**
+         * addToScore
+         *
+         * @param score is added to the current score of the target
+         */
         public void addToScore(int score){
             this.score+=score;
         }
     }
-
 }
